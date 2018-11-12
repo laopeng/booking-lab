f25f134a2eac491cec0f5c2996674b543f87ace9
pengyi <463508232@qq.com>
2018-11-12 15:24:00 +0800

    修改配置

--- src/main/java/ltd/hlmr/controller/WechatController.java
+++ src/main/java/ltd/hlmr/controller/WechatController.java
@@ -28,8 +28,8 @@ import okhttp3.Request;
 @Api(tags = "提供给微信调用的接口")
 public class WechatController {
 	private static final Logger logger = LoggerFactory.getLogger(WechatController.class);
-	private static final String appId = "wx9c6eb91e9f9af205";
-	private static final String secret = "d29875f42f0b03afa79c6cbd162b4b54";
+	private static final String appId = "wxcacab1dfed5bc106";
+	private static final String secret = "7c7f34c71a0af4c39afc8dd71d17d591";
 	@Autowired
 	private OkHttpClient okHttpClinet;
 
